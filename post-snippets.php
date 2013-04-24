e8d217b967dfb7c82306ab938c25a0c48abb5e40
Johan Steen <artstorm@gmail.com>
2013-04-24 22:01:12 +0700

    Updates init code.

--- post-snippets.php
+++ post-snippets.php
@@ -30,10 +30,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 spl_autoload_register('PostSnippets::autoload');
 
 /**
- * Init Singleton Class for Post Snippets.
+ * Init Singleton Class.
  *
- * @package Post Snippets
  * @author  Johan Steen <artstorm at gmail dot com>
+ * @link    http://johansteen.se/
  */
 class PostSnippets
 {
@@ -59,9 +59,7 @@ class PostSnippets
     }
 
     /**
-     * Constructor.
-     * Initializes the plugin by setting localization, filters, and
-     * administration functions.
+     * Initializes the plugin.
      */
     private function __construct()
     {
@@ -106,8 +104,6 @@ class PostSnippets
     /**
      * PSR-0 compliant autoloader to load classes as needed.
      *
-     * @since  2.1
-     *
      * @param  string  $classname  The name of the class
      * @return null    Return early if the class name does not start with the
      *                 correct prefix
@@ -133,11 +129,11 @@ class PostSnippets
     }
 
     /**
-     * Loads the text domain for translation
+     * Loads the plugin text domain for translation
      */
     public function textDomain()
     {
-        $domain = PostSnippets::TEXT_DOMAIN;
+        $domain = self::TEXT_DOMAIN;
         $locale = apply_filters('plugin_locale', get_locale(), $domain);
         load_textdomain(
             $domain,
@@ -722,6 +718,10 @@ function edOpenPostSnippets(myField) {
     // Environment Checks
     // -------------------------------------------------------------------------
 
+    // -------------------------------------------------------------------------
+    // Environment Checks
+    // -------------------------------------------------------------------------
+
     /**
      * Checks PHP and WordPress versions.
      */
@@ -730,14 +730,14 @@ function edOpenPostSnippets(myField) {
         // Check if PHP is too old
         if (version_compare(PHP_VERSION, self::MIN_PHP_VERSION, '<')) {
             // Display notice
-            add_action( 'admin_notices', array(&$this, 'phpVersionError') );
+            add_action('admin_notices', array(&$this, 'phpVersionError'));
             return false;
         }
 
         // Check if WordPress is too old
         global $wp_version;
         if (version_compare($wp_version, self::MIN_WP_VERSION, '<')) {
-            add_action( 'admin_notices', array(&$this, 'wpVersionError') );
+            add_action('admin_notices', array(&$this, 'wpVersionError'));
             return false;
         }
         return true;
@@ -750,9 +750,12 @@ function edOpenPostSnippets(myField) {
     {
         echo '<div class="error"><p><strong>';
         printf(
-            'Error: Post Snippets requires PHP version %1$s or greater.<br/>'.
+            'Error: %3$s requires PHP version %1$s or greater.<br/>'.
             'Your installed PHP version: %2$s',
-            self::MIN_PHP_VERSION, PHP_VERSION);
+            self::MIN_PHP_VERSION,
+            PHP_VERSION,
+            $this->getPluginName()
+        );
         echo '</strong></p></div>';
     }
 
@@ -763,9 +766,22 @@ function edOpenPostSnippets(myField) {
     {
         echo '<div class="error"><p><strong>';
         printf(
-            'Error: Post Snippets requires WordPress version %s or greater.',
-            self::MIN_WP_VERSION );
+            'Error: %2$s requires WordPress version %1$s or greater.',
+            self::MIN_WP_VERSION,
+            $this->getPluginName()
+        );
         echo '</strong></p></div>';
+    }
+
+    /**
+     * Get the name of this plugin.
+     *
+     * @return string The plugin name.
+     */
+    private function getPluginName()
+    {
+        $data = get_plugin_data(self::FILE);
+        return $data['Name'];
     }
 }
 
