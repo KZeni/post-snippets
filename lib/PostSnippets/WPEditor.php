24ebb8656410c237b6403d513544df7d5dfd14d0
Johan Steen <artstorm@gmail.com>
2013-05-10 00:13:46 +0700

    Code cleanup.

--- lib/PostSnippets/WPEditor.php
+++ lib/PostSnippets/WPEditor.php
@@ -30,27 +30,10 @@ class PostSnippets_WPEditor
         add_action('admin_init', array(&$this,'enqueueAssets'));
     }
 
-    /**
-     * Enqueues the necessary scripts and styles for the plugins
-     *
-     * @since       Post Snippets 1.7
-     */
-    public function enqueueAssets()
-    {
-        wp_enqueue_script('jquery-ui-dialog');
-        wp_enqueue_script('jquery-ui-tabs');
-        wp_enqueue_style('wp-jquery-ui-dialog');
-
-        # Adds the CSS stylesheet for the jQuery UI dialog
-        $style_url = plugins_url('/assets/post-snippets.css', PostSnippets::FILE);
-        wp_register_style('post-snippets', $style_url, false, '2.0');
-        wp_enqueue_style('post-snippets');
-    }
 
     // -------------------------------------------------------------------------
     // WordPress Editor Buttons
     // -------------------------------------------------------------------------
-
 
     /**
      * Add TinyMCE button.
@@ -151,6 +134,23 @@ class PostSnippets_WPEditor
     // -------------------------------------------------------------------------
     // JavaScript / jQuery handling for the post editor
     // -------------------------------------------------------------------------
+
+    /**
+     * Enqueues the necessary scripts and styles for the plugins
+     *
+     * @since       Post Snippets 1.7
+     */
+    public function enqueueAssets()
+    {
+        wp_enqueue_script('jquery-ui-dialog');
+        wp_enqueue_script('jquery-ui-tabs');
+        wp_enqueue_style('wp-jquery-ui-dialog');
+
+        # Adds the CSS stylesheet for the jQuery UI dialog
+        $style_url = plugins_url('/assets/post-snippets.css', PostSnippets::FILE);
+        wp_register_style('post-snippets', $style_url, false, '2.0');
+        wp_enqueue_style('post-snippets');
+    }
 
     /**
      * jQuery control for the dialog and Javascript needed to insert snippets into the editor
