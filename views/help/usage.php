e384aee6d6ba5dba6140ba5ad88a3877b82c8ff7
Johan Steen <artstorm@gmail.com>
2015-01-18 15:59:49 +0700

    Update usage

--- views/help/usage.php
+++ views/help/usage.php
@@ -1,32 +1,38 @@
-<h2><?php _e('Title', PostSnippets::TEXT_DOMAIN); ?></h2>
+<h2><?php _e('The Post Editor', $td); ?></h2>
+
+
+<h3><?php _e('Title', $td); ?></h3>
 <p>
-<?php _e('Give the snippet a title that helps you identify it in the post editor. This also becomes the name of the shortcode if you enable that option', PostSnippets::TEXT_DOMAIN); ?>
+<?php _e('Give the snippet a title that helps you identify it in the post editor. This also becomes the name of the shortcode if you enable that option.', $td); ?>
 </p>
 
-<h2><?php _e('Variables', PostSnippets::TEXT_DOMAIN); ?></h2>
+
+<h3><?php _e('Variables', $td); ?></h3>
 <p>
-<?php _e('A comma separated list of custom variables you can reference in your snippet. A variable can also be assigned a default value that will be used in the insert window by using the equal sign, variable=default.', PostSnippets::TEXT_DOMAIN); ?>
+<?php _e('A comma separated list of custom variables you can reference in your snippet. A variable can also be assigned a default value that will be used in the insert window by using the equal sign, variable=default.', $td); ?>
 </p>
 
 <p>
-    <strong><?php _e('Example', PostSnippets::TEXT_DOMAIN); ?></strong><br/>
-    <code>url,name,role=user,title</code>
+    <strong><?php _e('Example', $td); ?></strong>
+    <pre><code>url,name,role=user,title</code></pre>
 </p>
 
-<h2><?php _e('Snippet', PostSnippets::TEXT_DOMAIN); ?></h2>
+
+<h3><?php _e('Snippet', $td); ?></h3>
 <p>
-<?php _e('This is the block of text, HTML or PHP to insert in the post or as a shortcode. If you have entered predefined variables you can reference them from the snippet by enclosing them in {} brackets.', PostSnippets::TEXT_DOMAIN); ?>
+<?php _e('This is the block of text, HTML or PHP to insert in the post or as a shortcode. If you have entered predefined variables you can reference them from the snippet by enclosing them in {} brackets.', $td); ?>
 </p>
 
-<p><strong><?php _e('Example', PostSnippets::TEXT_DOMAIN); ?></strong><br/>
-<?php _e('To reference the variables in the example above, you would enter {url} and {name}. So if you enter this snippet:', PostSnippets::TEXT_DOMAIN); ?>
-<br/>
-<code>This is the website of &lt;a href="{url}"&gt;{name}&lt;/a&gt;</code>
-<br/>
-<?php _e('You will get the option to replace url and name on insert if they are defined as variables.', PostSnippets::TEXT_DOMAIN); ?>
-</p>
+<p><strong><?php _e('Example', $td); ?></strong></p>
+<p><?php _e('To reference the variables in the example above, you would enter {url} and {name}. So if you enter this snippet:', $td); ?></p>
+<p><?php _e('So if you enter this snippet:', $td); ?></p>
 
-<h2><?php _e('Description', PostSnippets::TEXT_DOMAIN); ?></h2>
+<pre><code>This is the website of &lt;a href="{url}"&gt;{name}&lt;/a&gt;</code></pre>
+
+<p><?php _e('You will get the option to replace url and name on insert if they are defined as variables.', $td); ?></p>
+
+
+<h3><?php _e('Description', $td); ?></h3>
 <p>
-<?php _e('An optional description for the Snippet. If filled out, the description will be displayed in the snippets insert window in the post editor.', PostSnippets::TEXT_DOMAIN); ?>
+<?php _e('An optional description for the Snippet. If filled out, the description will be displayed in the snippets insert window in the post editor.', $td); ?>
 </p>
