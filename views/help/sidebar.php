2ffe185d52e4b90b8586fd24932c7e2040bd8f17
davdebcom <david@davdeb.com>
2017-10-17 15:34:46 +0200

    Update Help > Sidebar URLs

--- views/help/sidebar.php
+++ views/help/sidebar.php
@@ -2,14 +2,15 @@
 <?php _e('Related:', 'post-snippets'); ?>
 </strong></p>
 
+<p><a href="//www.postsnippets.com" target="_blank"><?php
+		_e('Website', 'post-snippets');
+		?></a></p>
+
 <p><a href="http://wordpress.org/support/plugin/post-snippets" target="_blank"><?php
-_e('Support Forums', 'post-snippets');
+_e('Support', 'post-snippets');
 ?></a></p>
 
 <p><a href="https://github.com/artstorm/post-snippets" target="_blank"><?php
-_e('GitHub Repository', 'post-snippets');
+_e('GitHub', 'post-snippets');
 ?></a></p>
 
-<p><a href="https://code.bitbebop.com/donate/" target="_blank"><?php
-_e('Donate', 'post-snippets');
-?></a></p>
