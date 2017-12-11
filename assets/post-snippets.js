ad8f294ad419566c029e72da743e6c3417162480
sultann <manikdrmc@gmail.com>
2017-12-11 19:46:35 +0600

    add feature shortable snippets

--- assets/post-snippets.js
+++ assets/post-snippets.js
@@ -1,62 +1,104 @@
-jQuery(document).ready(function($)
-{
-  /**
-   * Loops through all snippets when page has loaded.
-   */
-  $("input[name$='_title']").each(function(index)
-  {
-    validateTitle($(this).prop('name'));
-  });
+jQuery(document).ready(function ($) {
+    /**
+     * Loops through all snippets when page has loaded.
+     */
+    $("input[name$='_title']").each(function (index) {
+        validateTitle($(this).prop('name'));
+    });
 
-  /**
-   * Listens for title text input change events.
-   */
-  $("input[name$='_title']").change(function (e)
-  {
-    validateTitle(e.target.name);
-  });
+    /**
+     * Listens for title text input change events.
+     */
+    $("input[name$='_title']").change(function (e) {
+        validateTitle(e.target.name);
+    });
 
-  /**
-   * Listens for shortcode checkbox change events.
-   */
-  $("input[name$='_shortcode']").change(function (e)
-  {
-    validateTitle(e.target.name);
-  });
+    /**
+     * Listens for shortcode checkbox change events.
+     */
+    $("input[name$='_shortcode']").change(function (e) {
+        validateTitle(e.target.name);
+    });
 
-  /**
-   * Handle title validation and managing CSS classes.
-   *
-   * @param  string name
-   *
-   * @return void
-   */
-  function validateTitle(name)
-  {
-    // Extract the snippet index number
-    var index = name.substring(0, name.indexOf('_'));
+    /**
+     * Handle title validation and managing CSS classes.
+     *
+     * @param  string name
+     *
+     * @return void
+     */
+    function validateTitle(name) {
+        // Extract the snippet index number
+        var index = name.substring(0, name.indexOf('_'));
 
-    var element = $("input[name$='"+index+"_title']");
-    var title = $("input[name$='"+index+"_title']").val();
+        var element = $("input[name$='" + index + "_title']");
+        var title = $("input[name$='" + index + "_title']").val();
+        $(element).removeClass('post-snippets-invalid');
+        $(element).next().remove('p');
 
-    $(element).removeClass('post-snippets-invalid');
-    $(element).next().remove('p');
-
-    if ($('#'+index+'_shortcode').prop('checked') && !isTitleValid(title)) {
-      $(element).addClass('post-snippets-invalid');
-      $(element).after("<p><em><font color='red'>"+post_snippets.invalid_shortcode+"</font></em></p>");
+        if ($('#' + index + '_shortcode').prop('checked') && !isTitleValid(title)) {
+            $(element).addClass('post-snippets-invalid');
+            $(element).after("<p><em><font color='red'>" + post_snippets.invalid_shortcode + "</font></em></p>");
+        }
     }
-  }
 
-  /**
-   * Determine if a title is shortcode valid.
-   *
-   * @param  string  title
-   *
-   * @return boolean
-   */
-  function isTitleValid(title)
-  {
-    return !Boolean(title.match(/[<>&/\[\]\x00-\x20]/gi));
-  }
+    /**
+     * Determine if a title is shortcode valid.
+     *
+     * @param  string  title
+     *
+     * @return boolean
+     */
+    function isTitleValid(title) {
+        return !Boolean(title.match(/[<>&/\[\]\x00-\x20]/gi));
+    }
+
+    /**
+     * Bulk check/uncheck
+     */
+    $('.check-column input').on('change', function () {
+       if( $(this).is(":checked") ){
+            $('.post-snippets-data input[name^="checked"]').each(function(){
+                $(this).attr('checked', true);
+            });
+       }else{
+           $('.post-snippets-data input[name^="checked"]').each(function(){
+               $(this).attr('checked', false);
+           });
+       }
+    });
+    /**
+     * Update title real time
+     */
+    $('input.post-snippet-title').on('keyup', function () {
+        var val = $(this).val();
+        $(this).closest('.post-snippets-item').find('span.post-snippet-title').text(val);
+    });
+
+    /**
+     * toggle folding
+     */
+    $('.toggle-post-snippets-data').on('click', function () {
+        $(this).closest('.post-snippets-item').find('.post-snippets-data').slideToggle('fast');
+        $(this).closest('.post-snippets-item').toggleClass('open');
+        return false;
+    });
+    /**
+     * Make shortable list
+     */
+    $(".post-snippets").sortable({
+        handle: ".handle",
+        items: ".post-snippets-item",
+        update: function () {
+            var order = $('.post-snippets').sortable("toArray",{option:"sort", attribute:"data-order"});
+            var data = {
+                'action': 'update_post_snippets_order',
+                'order': order
+            };
+            $.post(ajaxurl,data,function (res) {
+                console.log(res);
+            });
+        }
+    });
+    $(".post-snippets").disableSelection();
 });
